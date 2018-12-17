cea21f5fc5c7aac4ed2025fd1ff6d46808d69ac4
Prasad <psam012@aucklanduni.ac.nz>
2018-12-17 15:46:25 +1300

    Updating google drive api scopes

--- gsuites_api_access.py
+++ gsuites_api_access.py
@@ -14,7 +14,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+SCOPES = 'https://www.googleapis.com/auth/drive'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 
